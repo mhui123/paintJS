83f4345008b2b1a965f87e5b206d3135b7d9a590
mhui12311 <82005295+mhui12311@users.noreply.github.com>
2021-11-17 11:04:36 +0900

    캔버스에 그리기 및 색상변경

--- app.js
+++ app.js
@@ -1,30 +1,55 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const clearBtn = document.getElementById("jsClear");
+const colors = document.getElementsByClassName("jsColor");
+
+//canvas size
+canvas.width = 700;
+canvas.height = 700;
+
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 let painting = false;
 
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
 
-    console.log(x, y);
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function startPainting(){
+    painting = true;
 }
 
 function stopPainting(){
     painting = false;
 }
 
-function onMouseUp(event){
-    painting = false;
-}
-
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if(canvas) {
     //mouse on canvas
     canvas.addEventListener("mousemove", onMouseMove);
     //mouse click down event
-    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+clearBtn.addEventListener("click", () => {
+    ctx.clearRect(0,0, 700, 700);
+})
